7f8a8c9d8f34a097ca9f9f35fec40dda56aab660
tomwyr <tomwyr@hotmail.com>
2021-09-08 20:51:24 +0200

    Remove unnecessary client feature exports

--- src/templates/client-exports.template.ts
+++ src/templates/client-exports.template.ts
@@ -8,20 +8,12 @@ export function getClientExportsTemplate(
   const snakeCaseFeatureName = changeCase.snakeCase(featureName.toLowerCase())
   const snakeCaseWidgetSuffix = changeCase.snakeCase(widgetSuffix.toLowerCase())
 
-  const widgetName = `${snakeCaseFeatureName}_${snakeCaseWidgetSuffix}`
-
-  let connectorName = snakeCaseFeatureName
+  let connectorName = snakeCaseFeatureName;
   if (connectorIncludeWidgetSuffix) {
     connectorName += `_${snakeCaseWidgetSuffix}`
   }
   connectorName += "_connector"
 
-  const viewModelName = `${snakeCaseFeatureName}_view_model`
-  const viewModelFactoryName = `${snakeCaseFeatureName}_view_model_factory`
-
-  return `export '${widgetName}.dart';
-export '${connectorName}.dart';
-export '${viewModelName}.dart';
-export '${viewModelFactoryName}.dart';
-`
+  return `export '${connectorName}.dart';
+`;
 }
